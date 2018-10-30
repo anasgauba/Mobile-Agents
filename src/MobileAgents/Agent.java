4a5e9e1064febab1bcfa4f31fa47e2d37003847b
Anas Gauba <anas.farooq71@gmail.com>
2018-10-30 11:14:38 -0600

    Agent can walk, can clone itself on live neighbors. Have to test

--- src/MobileAgents/Agent.java
+++ src/MobileAgents/Agent.java
@@ -1,10 +1,32 @@
 package MobileAgents;
 
 public class Agent extends Thread{
-    public Agent(){
+    private Node currentNode;
+    private boolean tasks;
+//    private boolean killed;
+    public Agent(Node node, boolean task){
+        this.currentNode = node;
+        this.tasks = task;
+//        this.killed = false;
         run();
     }
     @Override
     public void run(){
+        while (tasks) {
+            currentNode.passAgent();
+            if (currentNode.getStatus().equals(Status.YELLOW)) {
+                currentNode.sendCloneAgent();
+                tasks = false;
+            }
+        }
+        if (currentNode.getStatus().equals(Status.RED)) {
+            currentNode.neigborStausChanged(currentNode);
+            currentNode.sendCloneAgent();
+//                killed = true;
+        }
     }
+
+//    public boolean getAgentKilledStatus() {
+//        return killed;
+//    }
 }
